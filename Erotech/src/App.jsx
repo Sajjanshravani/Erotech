19a6fce98df45add2f4c605b47db1f5b861ee94e
Vadlamudi Bhuvan <bhuvanvadlamudi143@gmail.com>
2024-04-24 16:46:35 +0530

    home page link

--- Erotech/src/App.jsx
+++ Erotech/src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
 import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Pages/Home";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <NavBar />
-        <Routes>{/* <Route path="/" element={< />} /> */}</Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
         <Footer />
       </BrowserRouter>
     </div>
