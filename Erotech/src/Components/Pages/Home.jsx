2a1e9360939565c1f43de229e3d7ce7ad13e66b8
Vadlamudi Bhuvan <bhuvanvadlamudi143@gmail.com>
2024-04-25 11:48:08 +0530

    animations

--- Erotech/src/Components/Pages/Home.jsx
+++ Erotech/src/Components/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Slider from "react-slick";
 import "./CSS/Home.css";
 import Homebanner1 from "../Assets/Homebanner1.png";
@@ -24,6 +24,72 @@ const Home = () => {
     autoplay: true,
     autoplaySpeed: 3000,
   };
+  const [isVisible, setIsVisible] = useState(false);
+  const investingRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 0.5 }
+    );
+
+    if (investingRef.current) {
+      observer.observe(investingRef.current);
+    }
+
+    return () => {
+      if (investingRef.current) {
+        observer.unobserve(investingRef.current);
+      }
+    };
+  }, []);
+
+  const [isSection3Visible, setIsSection3Visible] = useState(false);
+  const section3Ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsSection3Visible(entry.isIntersecting);
+      },
+      { threshold: 0.5 }
+    );
+
+    if (section3Ref.current) {
+      observer.observe(section3Ref.current);
+    }
+
+    return () => {
+      if (section3Ref.current) {
+        observer.unobserve(section3Ref.current);
+      }
+    };
+  }, []);
+
+  const [isSection4Visible, setIsSection4Visible] = useState(false);
+  const section4Ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsSection4Visible(entry.isIntersecting);
+      },
+      { threshold: 0.5 }
+    );
+
+    if (section4Ref.current) {
+      observer.observe(section4Ref.current);
+    }
+
+    return () => {
+      if (section4Ref.current) {
+        observer.unobserve(section4Ref.current);
+      }
+    };
+  }, []);
+
   return (
     <>
       <div className="HomeBanner">
@@ -68,7 +134,10 @@ const Home = () => {
           </div>
         </Slider>
       </div>
-      <div className="investing">
+      <div
+        ref={investingRef}
+        className={`investing ${isVisible ? "visible" : ""}`}
+      >
         <div className="invest-title">
           <p>STUDY WITH YOUR FAVORITE TUTORS</p>
           <h2>Investing In Yourself</h2>
@@ -104,9 +173,16 @@ const Home = () => {
         <h2>Explore Categories</h2>
         <IconCarousel />
       </div>
-      <div className="section3">
+      <div
+        ref={section3Ref}
+        className={`section3 ${isSection3Visible ? "visible" : ""}`}
+      >
         <div className="row">
-          <div className="col-lg-7 col-sm-12 section3-Image">
+          <div
+            className={`col-lg-7 col-sm-12 section3-Image ${
+              isSection3Visible ? "visible" : ""
+            }`}
+          >
             <img
               src={pentagon}
               alt="Section3Vectors"
@@ -134,15 +210,19 @@ const Home = () => {
             />
             <div className="GrayImg">IMAGE</div>
           </div>
-          <div className="col-lg-5 col-sm-12 section3-Description">
+          <div
+            className={`col-lg-5 col-sm-12 section3-Description ${
+              isSection3Visible ? "visible" : ""
+            }`}
+          >
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
+              Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
               electronic typesetting, remaining essentially unchanged. Lorem
-              Ipsum is simply dummy text of the printing and typesetting
+              Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
@@ -150,42 +230,53 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className="section4">
+      <div
+        ref={section4Ref}
+        className={`section4 ${isSection4Visible ? "visible" : ""}`}
+      >
         <div className="row">
-          <div className="col-lg-5 col-sm-12 section4-Description">
+          <div
+            className={`col-lg-5 col-sm-12 section4-Description ${
+              isSection4Visible ? "visible" : ""
+            }`}
+          >
             <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
+              Lorem Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
               electronic typesetting, remaining essentially unchanged. Lorem
-              Ipsum is simply dummy text of the printing and typesetting
+              Ipsum is simply dummy text of the printing and typesetting
               industry. Lorem Ipsum has been the industry's standard dummy text
               ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book.
             </p>
           </div>
-          <div className="col-lg-7 col-sm-12 section4-Boxes">
+          <div
+            className={`col-lg-7 col-sm-12 section4-Boxes ${
+              isSection4Visible ? "visible" : ""
+            }`}
+          >
             <div className="leftBoxes">
-              <div className="section4Box">
+              <div className="section4Box section4Box1">
                 <h2>1.926</h2>
                 <h3>Finished Sessions</h3>
                 <p>Get Some Variety Guidance And For Your Future Career.</p>
               </div>
-              <div className="section4Box">
+              <div className="section4Box section4Box2">
                 <h2>3.092+</h2>
                 <h3>Enrolled Learners</h3>
                 <p>GMake Reservation With Consultant To Get Dream Career.</p>
               </div>
             </div>
             <div className="rightBoxes">
-              <div className="section4Box">
+              <div className="section4Box section4Box3">
                 <h2>100%</h2>
                 <h3>Satisfaction Rate</h3>
                 <p>Develop Skill For Career Various Science & Language.</p>
               </div>
-              <div className="section4Box">
+              <div className="section4Box section4Box4">
                 <h2>20k+</h2>
                 <h3>Online Instructors</h3>
                 <p>Learn From Anywhere In The World With Any Devices.</p>
