b120c59474d213ac16d35d724a7ab065cf6b3977
maheshGootlaDev <maheshgootla9@gmail.com>
2024-04-26 13:16:32 +0530

    mid part

--- Erotech/src/Components/ContactUs/ContactUs.jsx
+++ Erotech/src/Components/ContactUs/ContactUs.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import Homebanner1 from "../Assets/Homebanner1.png";
 import ladysittingonbooks from '../Assets/ladysittingonbooks.png';
-import contactus from '../Assets/contactus.png';
+import Frame7 from '../Assets/Frame7.png';
+import location from '../Assets/location.png';
+import contactCard from '../Assets/contactCard.png';
 import "./ContactUs.css";
 
 
@@ -11,19 +13,32 @@ function ContactUs(){
 
     return(
         <>
-       
-            <div className="HomeSlide">
-                <img src={contactus} alt="home-banner1" />
+            <div className="HomeSlide contact-bg">
                 <div className="BannerText">
                     <p>Get Personalized Recommendations</p>
                     <h2>Contact Us</h2>
                 </div>
             </div>
+            <div className='address-section'>
+                <div>
+                    <img src={location} alt='locaiont'/>
+                    <h3>Our Address</h3>
+                    <p>
+                        #103, Rajamma Towers, Jayanagar Rd, Kukatpally Housing Board Colony, Hyderabad, Telangana 500072
+                    </p>
+                </div>
+                <div>
+                    <img src={contactCard} alt='contact'/>
+                    <h3>
+                        Contact Us
+                    </h3>
+                    <p>
+                        096763 27118
+                    </p>
+                </div>
+            </div>
             <div className="form-section">
                 <img src={ladysittingonbooks} alt="contactUs"/>
-                <form>
-                    
-                </form>
             </div>
             <div className="" style={{marginTop:"100px"}}>
               <p>START TODAY FOR GETTING ONLINE CERTIFICATION</p>
